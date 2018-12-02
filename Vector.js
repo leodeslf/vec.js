b2821840d8beb4e0eeb1c769b69de7b6a1295563
Leo <leodesouzauy@hotmail.com>
2018-12-01 21:59:01 -0300

    Update Vector.js

--- Vector.js
+++ Vector.js
@@ -240,18 +240,3 @@ class Vector {
 }
 
 export default Vector;
-
-/**
- * 
- * Features:
- * - Static methods:
- *   Add, subtract, multiply, divide, (euclidian, manhattan and 
- *   chebyshev) distance, create vector by polar coordinates.
- * 
- * - Instance methods:
- *   Add, subtract, multiply, divide, normalize, get/set magnitude,
- *   limit, get angle.
- * 
- * Leonardo de Souza Leal - 10/2018 - Last update: 11/2018
- * 
- */
